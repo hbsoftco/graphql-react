3a0fc175ef3bed9eec8517b21424d1cb7c221b9e
Hossein <hossein.bajan@gmail.com>
2024-05-08 20:43:21 +0330

    Add AllCharacters query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AllCharacters = gql`
+  {
+    characters(page: 1) {
+      results {
+        name
+        gender
+        id
+        image
+      }
+    }
+  }
+`;
+
+interface User {
+  name: string;
+  gender: string;
+  id: number;
+  image: string;
+}
+
+const App = () => {
+  const { loading, error, data } = useQuery(AllCharacters);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>Some error happened</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hey dude</h1>
+      {data.characters.results.map((char: User) => {
+        return <p key={char.id}>{char.name}</p>;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
