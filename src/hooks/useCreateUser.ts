858267a4c109523b013f337f29956cf171ca330b
Hossein <hossein.bajan@gmail.com>
2024-05-10 21:59:47 +0330

    Update cache after mutation

--- src/hooks/useCreateUser.ts
+++ src/hooks/useCreateUser.ts
@@ -8,16 +8,44 @@ interface UserInput {
   };
 }
 
+interface CreateUserResponse {
+  createUser: User;
+}
+
 const CREATE_USER = gql`
   mutation CreateUser($userInput: UserInput!) {
     createUser(userInput: $userInput) {
       id
+      email
+      username
     }
   }
 `;
 
 const useCreateUser = () => {
-  return useMutation<User, UserInput>(CREATE_USER);
+  return useMutation<CreateUserResponse, UserInput>(CREATE_USER, {
+    update(cache, { data }) {
+      if (data?.createUser) {
+        cache.modify({
+          fields: {
+            users(existingUsers = []) {
+              const newUserRef = cache.writeFragment({
+                data: data.createUser,
+                fragment: gql`
+                  fragment NewUser on User {
+                    id
+                    email
+                    username
+                  }
+                `,
+              });
+              return [...existingUsers, newUserRef];
+            },
+          },
+        });
+      }
+    },
+  });
 };
 
 export { useCreateUser };
